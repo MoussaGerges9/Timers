fd3257d99cfcff1eca1919e9264e38ac09dbaf63
Moussa Gerges <moussagerges9@gmail.com>
2021-11-24 22:31:30 +0200

    Upload the project

---
+++ Timers/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Timers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.dropList = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.confirmBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmBtn.Location = new System.Drawing.Point(91, 152);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(143, 50);
+            this.confirmBtn.TabIndex = 0;
+            this.confirmBtn.Text = "Confirm";
+            this.confirmBtn.UseVisualStyleBackColor = true;
+            this.confirmBtn.Click += new System.EventHandler(this.confirmBtn_Click);
+            // 
+            // dropList
+            // 
+            this.dropList.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dropList.FormattingEnabled = true;
+            this.dropList.Items.AddRange(new object[] {
+            "",
+            "Sleep",
+            "Shut Down",
+            "Restart",
+            "Open Whatsapp"});
+            this.dropList.Location = new System.Drawing.Point(58, 91);
+            this.dropList.Name = "dropList";
+            this.dropList.Size = new System.Drawing.Size(200, 37);
+            this.dropList.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(72, 33);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(174, 34);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(278, 103);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Hide";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 243);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dropList);
+            this.Controls.Add(this.confirmBtn);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button confirmBtn;
+        private System.Windows.Forms.ComboBox dropList;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Timers/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Timers
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(dropList.Text))
+            {
+                if (checkBox1.CheckState == CheckState.Checked)
+                    this.ShowInTaskbar = false; // hide the application icon from the taskbar
+
+                this.WindowState = FormWindowState.Minimized; // Minimize the application after confirming the command
+
+                //start the Timer
+                timer.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create an interval of 1 sec to call the function every 1 second
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed; //The name of the function that we are going to create
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime desiredTime = dateTimePicker1.Value;
+
+            Invoke(new Action(() =>
+            {
+                //Compare the current time with the desired Time
+                if (currentTime.Hour == desiredTime.Hour && currentTime.Minute == desiredTime.Minute &&
+                    currentTime.Second == desiredTime.Second)
+                {
+                    timer.Stop();
+                    Run(); // the function that does all the operations
+                    Application.Exit();
+                }
+            }));
+
+        }
+
+
+        public void Run()
+        {
+            switch (dropList.Text)
+            {
+                case "Shut Down":
+                    System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+                    break;
+
+                case "Restart":
+                    System.Diagnostics.Process.Start("shutdown", "/r /t 0");
+                    break;
+
+                case "Sleep":
+                    Application.SetSuspendState(PowerState.Suspend, false, true);
+                    break;
+
+                case "Open Whatsapp":
+                    System.Diagnostics.Process.Start("https://web.whatsapp.com/");
+                    break;
+
+                // Of course you can add your own operation or task here or you can add your favourite website to open it!
+
+
+            }
+
+        }
+
+
+    }
+}
